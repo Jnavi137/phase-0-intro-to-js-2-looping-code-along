01a40550ab42720a5592372633d19cb92ef9ad8d
Jed Navida <Navidajed@gmail.com>
2022-06-08 18:18:32 -0400

    commit tree 21a93c93dc04bcfd537fadb420c1aee25c23d02e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+function writeCards(array, event) {
+    const cards = []
+  for (let p = 0; p < array.length; p++) {
+     cards.push(`Thank you, ${array[p]}, for the wonderful ${event} gift!`);
+  }
+  return cards
+}
+function countDown() {
+    let q = 10;
+    while (q >= 0) {
+        console.log(q--);
+    }
+}
